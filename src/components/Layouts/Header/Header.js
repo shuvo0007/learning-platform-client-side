34c2a45fe2905eab2d3b09e26b4a0e02351e8cc9
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-10-26 20:47:44 +0600

    log in with google

--- src/components/Layouts/Header/Header.js
+++ src/components/Layouts/Header/Header.js
@@ -1,8 +1,19 @@
+import { Button } from "flowbite-react";
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthProvider/AuthProvider";
+import { FaRegUser } from "react-icons/fa";
 
 const Header = () => {
-  const { user } = useContext(AuthContext);
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {
+
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <div>
@@ -18,20 +29,45 @@ const Header = () => {
               Digital Educations
             </span>
           </a>
+
+          {/* log in checking if logged in then show name and photo otherwise show login and register button  */}
           <div className="flex items-center">
-            <a
-              href="/"
-              className="border-2 border-red-400 p-2 rounded-lg mx-2 text-base  font-medium text-blue-600 dark:text-blue-500 hover:bg-red-500 hover:text-white"
-            >
-              Login
-            </a>
-            <p>{user?.displayName}</p>
-            <a
-              href="/"
-              className="border-2 border-red-400 p-2 rounded-lg mx-2 text-base font-medium text-blue-600 dark:text-blue-500 hover:bg-red-500 hover:text-white"
-            >
-              Registration
-            </a>
+            {user ? (
+              <div className="flex items-center">
+                <p className="m-5 text-xl">{user?.displayName}</p>
+                {user.photoURL ? (
+                  <>
+                    <img
+                      className="rounded-full w-16"
+                      src={user.photoURL}
+                      alt=""
+                    />
+                    <button
+                      onClick={handleLogOut}
+                      type="button"
+                      className="mx-5 text-lg text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2"
+                    >
+                      LogOut
+                    </button>
+                  </>
+                ) : (
+                  <FaRegUser className=""></FaRegUser>
+                )}
+              </div>
+            ) : (
+              <>
+                <Link to={"/login"}>
+                  <Button className="border-2 border-red-400 p-2 rounded-lg mx-2 text-base  font-medium text-blue-600 dark:text-blue-500 hover:bg-red-500 hover:text-white">
+                    Login
+                  </Button>
+                </Link>
+                <Link to={"/registration"}>
+                  <Button className="border-2 border-red-400 p-2 rounded-lg mx-2 text-base font-medium text-blue-600 dark:text-blue-500 hover:bg-red-500 hover:text-white">
+                    Registration
+                  </Button>
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </nav>
@@ -42,34 +78,41 @@ const Header = () => {
               <li>
                 <a
                   href="/"
-                  className="text-gray-900 dark:text-white hover:underline"
-                  aria-current="page"
+                  className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
                 >
-                  Courses
+                  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    Courses
+                  </span>
                 </a>
               </li>
               <li>
                 <a
                   href="/"
-                  className="text-gray-900 dark:text-white hover:underline"
+                  className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
                 >
-                  FAQ
+                  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    FAQ
+                  </span>
                 </a>
               </li>
               <li>
                 <a
                   href="/"
-                  className="text-gray-900 dark:text-white hover:underline"
+                  className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
                 >
-                  Blog
+                  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    Blog
+                  </span>
                 </a>
               </li>
               <li>
                 <a
                   href="/"
-                  className="text-gray-900 dark:text-white hover:underline"
+                  className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
                 >
-                  (Toggle Theme Button)
+                  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    (Toggle Theme Button)
+                  </span>
                 </a>
               </li>
             </ul>
--- src/components/context/AuthProvider/AuthProvider.js
+++ src/components/context/AuthProvider/AuthProvider.js
@@ -1,12 +1,40 @@
 import React from "react";
 import { createContext } from "react";
-
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../../../firebase/firebase.init";
+import { useState } from "react";
+import { useEffect } from "react";
 export const AuthContext = createContext();
 
+const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
-  const user = { displayName: "Mir Vai" };
+  const [user, setUser] = useState(null);
 
-  const authInfo = { user };
+  const providerLogin = (provider) => {
+    return signInWithPopup(auth, provider);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser);
+      setUser(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authInfo = { user, providerLogin, logOut };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
